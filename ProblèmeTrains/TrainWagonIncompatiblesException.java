c61f7c237756320aff24dfed8462059e039d65e7
Mohamed <124465334+maybeidkwhoami@users.noreply.github.com>
2024-01-09 23:37:40 +0000

    Delete ProblèmeTrains directory

--- ProblèmeTrains/TrainWagonIncompatiblesException.java
+++
@@ -1,2 +0,0 @@
-public class TrainWagonIncompatiblesException extends Exception{
-public TrainWagonIncompatiblesException(String message) { super(message); } }
--- ProblèmeTrains/TypeTrain.java
+++
@@ -1,1 +0,0 @@
-public enum TypeTrain { Marchandise, Passagers, PassagersMarchandise, Minerai }
